731a5ae01aff08fd949912b7825def44fca11fbb
Clémentine Bonneau <Clementine@MacBook-Pro-de-Clementine.local>
2020-11-23 15:19:08 +0100

    structure agent et usersocket

--- Clavardage/src/Agent.java
+++ Clavardage/src/Agent.java
@@ -1,4 +1,51 @@
+import java.util.ArrayList;
 
 public class Agent extends Subject {
+	private MyMap<String,User> mapUsernames;
+	private MyMap<User,MessageEmissionObserver> mapObserver;
+	
+	public boolean chooseUsername(String name) {
+		
+	}
+	
+	public ArrayList<String> viewActiveUsernames(){
+		
+	}
+	
+	public ArrayList<Message> getMessageHistory(String username){
+		
+	}
+	
+	public void sendMessage(Message message) {
+		
+	}
+	
+	private User nameResolve(String username) {
+		
+	}
+	
+	private void checkUsernameAvailability(String username) {
+		
+	}
+	
+	public void userDisconnect(User user) {
+		
+	}
+	
+	public void newActiveUserSocket(Socket socket) {
+		
+	}
+	
+	public void receiveFile(File file) {
+		
+	}
+	
+	public void receiveText(Text text) {
+		
+	}
+
+	public void receiveImage(Image image) {
+	
+	}
 
 }
--- Clavardage/src/UserSocket.java
+++ Clavardage/src/UserSocket.java
@@ -1,4 +1,42 @@
-
 public class UserSocket extends Subject {
+	
+/*-----------------------Attributs-------------------------*/
 
+	private Socket socket;
+	private User user;
+	private MessageReceptionObserver messageReceptionObserver;
+	
+/*-----------------------Guetteurs-------------------------*/
+	
+	
+	public User getUser() {
+		
+	}
+	
+/*-----------------------Méthodes-------------------------*/
+	
+	public void informAllUsersNewUsername(String name) {
+		
+	}
+	
+	public void sendImage(Image image) {
+		
+	}
+	
+	public void sendText(Text text) {
+		
+	}
+	
+	public void sendFile(File file){
+		
+	}
+	
+	public void sendDate(Date date) {
+		
+	}
+	
+	
+	public void receiveMessage() {
+		
+	}
 }
